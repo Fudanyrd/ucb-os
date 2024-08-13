75fc458b2f13b7db915c5f5f3bfcd587079f8ad0
Fudanyrd <143480204+Fudanyrd@users.noreply.github.com>
2024-08-13 19:37:25 +0800

    [EMERGENCY] paper is due soon, schedule

--- src/filesys/inode.c
+++ src/filesys/inode.c
@@ -8,6 +8,10 @@
 #include "threads/malloc.h"
 #include "threads/synch.h"
 
+#include "common.h"
+
+#ifndef FILESYS
+
 /** Identifies an inode. */
 #define INODE_MAGIC 0x494e4f44
 
@@ -390,3 +394,422 @@ inode_length (const struct inode *inode)
 {
   return inode->data.length;
 }
+
+#else  /**< Add your own inode impl! */
+#include "bio.h"
+
+/** inode magic number */
+#define INODE_MAGIC 0x10203040
+
+/** invalid inode position */
+#define INODE_INVALID (-1)
+
+/** 
+ * cication: <https://github.com/mit-pdos/xv6-riscv/blob/riscv/kernel/fs.h> 
+ */
+
+enum inode_type {
+  /**< Invalid type */
+  INODE_NULL = 0,
+  /**< File */
+  INODE_FILE,
+  /**< Directory */
+  INODE_DIR,
+};
+
+/** On-disk inode.
+   Must be exactly BLOCK_SECTOR_SIZE bytes long. */
+struct inode_disk
+  {
+    short type;           /**< Type of file */
+    short nlink;          /**< Number of links to inode */
+    off_t size;           /**< Size of file (bytes) */
+    int addrs[125];       /**< Data block addresses */
+    unsigned magic;       /**< Magic number */
+  };
+
+/**< size of sum of direct sectors */
+#define DIRECT_SIZE (123 * BLOCK_SECTOR_SIZE)
+
+/**< size of single indirect blocks */
+#define SINGLE_INDIR_SIZE (128 * BLOCK_SECTOR_SIZE)
+
+/**< size of doubly indirect blocks */
+#define DOUBLY_INDIR_SIZE (128 * SINGLE_INDIR_SIZE)
+
+/**< maximum size of file */
+#define MAXFILE (DIRECT_SIZE + SINGLE_INDIR_SIZE + DOUBLY_INDIR_SIZE)
+
+/** In-memory inode. */
+struct inode 
+  {
+    struct list_elem elem;              /**< Element in inode list. */
+    block_sector_t sector;              /**< Sector number of disk location. */
+    int open_cnt;                       /**< Number of openers. */
+    bool removed;                       /**< True if deleted, false otherwise. */
+    int deny_write_cnt;                 /**< 0: writes ok, >0: deny writes. */
+    struct inode_disk *data;            /**< Inode content. */
+    struct lock lk;                     /**< inode lock */
+  };
+
+/** Indirect block */
+struct indirect_block 
+  {
+    block_sector_t addrs[128];          /**< Address to other blocks. */
+  };
+
+/** Deallocate all sectors occupied by an inode. */
+static void
+inode_deallocate (struct inode *ino)
+{
+  if (ino == NULL)
+    PANIC ("invalid argument");
+  ASSERT (lock_held_by_current_thread (&ino->lk));
+
+  /* Fetch and pin the sector */
+  const struct inode_disk *di = bio_read (ino->sector); 
+  if (di == NULL) {
+    PANIC ("buffer full");
+  }
+  if (!bio_pin_sec (di)) {
+    PANIC ("pin sec");
+  }
+
+  /* For each direct page, destroy */
+  for (int i = 0; i < 123; ++i)
+    {
+      if (di->addrs[i] != INODE_INVALID) {
+        free_map_release (di->addrs[i], 1U);
+      }
+    }
+  
+  /* Free primary indirect page */
+  if (di->addrs[123] != INODE_INVALID)
+    {
+      const struct indirect_block *ind = bio_read (di->addrs[123]);
+      if (!bio_pin_sec (ind))
+        PANIC ("bio pin");
+      for (int i = 0; i < 128; ++i) {
+        if (ind->addrs[i] != INODE_INVALID)
+          free_map_release (ind->addrs[i], 1U);
+      }
+
+      if (!bio_free_sec (ind))
+        PANIC ("bio free");
+      free_map_release (di->addrs[123], 1U);
+    }
+
+  /* Free doubly indirect blocks. */
+  if (di->addrs[124] != INODE_INVALID)
+    {
+      const struct indirect_block *first = bio_read (di->addrs[124]);
+      if (!bio_pin_sec (first))
+        PANIC ("bio pin");
+      const struct indirect_block *second;
+
+      for (int i = 0; i < 128; ++i) {
+        if (first->addrs[i] == INODE_INVALID)
+          continue;
+        
+        second = bio_read (first->addrs[i]);
+        if (!bio_pin_sec (second))
+          PANIC ("bio pin");
+
+        for (int k = 0; k < 128; ++k) {
+          if (second->addrs[k] != INODE_INVALID)
+            free_map_release (second->addrs[k], 1U);
+        }
+        
+        if (!bio_free_sec (second))
+          PANIC ("bio free");
+        free_map_release (first->addrs[i], 1U);
+      }
+
+      if (!bio_free_sec (first))
+        PANIC ("bio free");
+      free_map_release (di->addrs[124], 1U);
+    }
+
+  /* Finish. */ 
+  if (!bio_free_sec (di)) {
+    PANIC ("free sec");
+  }
+  free_map_release (ino->sector, 1U);
+}
+
+/** Seek a position of inode */
+static const char *
+inode_seek_read (struct inode *ino, const struct inode_disk *di, off_t offset)
+{
+  ASSERT (lock_held_by_current_thread (&ino->lk));
+  if (offset >= MAXFILE) /* Seeking outside of largest file */
+    return NULL;
+
+  /* By specification, will use ino->data to communicate */
+  if (di == NULL)
+    PANIC ("ino->data is invalid");
+
+  if (offset < DIRECT_SIZE) {
+    /* Seek into direct size. */
+    int idx __attribute__((unused)) = offset / BLOCK_SECTOR_SIZE;
+
+    return NULL;
+  }
+}
+
+/** List of open inodes, so that opening a single inode twice
+   returns the same `struct inode'. */
+static struct list open_inodes;
+
+/** Protect against concurrent access to inode list */
+static struct lock inode_list_lock;
+
+/** WARNING: to avoid deadlock, you must hold inode_list_lock BEFORE holding
+   locks belonging to an inode. */
+
+/** Initializes the inode module. */
+void
+inode_init (void) 
+{
+  /* dinode must be exactly BLOCK_SECTOR_SIZE. */
+  STATIC_ASSERT (sizeof (struct inode_disk) == BLOCK_SECTOR_SIZE);
+  /* Indirect block must be exactly block sector size. */
+  STATIC_ASSERT (sizeof (struct indirect_block) == BLOCK_SECTOR_SIZE);
+  /* Must support file of 8MB or larger */
+  STATIC_ASSERT (MAXFILE >= (8 * 1024 * 1024));
+
+  list_init (&open_inodes);
+  lock_init (&inode_list_lock);
+}
+
+/** Initializes an inode with LENGTH bytes of data and
+   writes the new inode to sector SECTOR on the file system
+   device.
+   Returns true if successful.
+   Returns false if memory or disk allocation fails. */
+bool 
+inode_create (block_sector_t sec, off_t size)
+{
+  /* Fetch and pin the sector. */
+  struct inode_disk *di = bio_write (sec);
+  if (di == NULL)
+    return false;
+  if (!bio_pin_sec (di)) {
+    PANIC ("inode_create pin");
+  }
+
+  di->type = INODE_FILE;
+  di->size = size;
+  /* TODO: set a reasonable nlink. */
+  di->nlink = 1;
+
+  for (int i = 0; i < 125; ++i)
+    {
+      di->addrs[i] = INODE_INVALID;
+    }
+  di->magic = INODE_MAGIC;
+
+  /* Unpin the page, done. */
+  if (!bio_unpin_sec (di)) {
+    PANIC ("inode_create unpin");
+  }
+  return true;
+}
+
+/** Reads an inode from SECTOR
+   and returns a `struct inode' that contains it.
+   Returns a null pointer if memory allocation fails. */
+struct inode *
+inode_open (block_sector_t sector)
+{
+  struct list_elem *e;
+  struct inode *inode;
+  lock_acquire (&inode_list_lock);
+
+  /* Check whether this inode is already open. */
+  for (e = list_begin (&open_inodes); e != list_end (&open_inodes);
+       e = list_next (e)) 
+    {
+      inode = list_entry (e, struct inode, elem);
+      if (inode->sector == sector) 
+        {
+          inode_reopen (inode);
+          lock_release (&inode_list_lock);
+          return inode; 
+        }
+    }
+
+  /* Allocate memory. */
+  inode = malloc (sizeof *inode);
+  if (inode == NULL) {
+    lock_release (&inode_list_lock);
+    return NULL;
+  }
+
+  /* Initialize. */
+  list_push_front (&open_inodes, &inode->elem);
+  inode->sector = sector;
+  inode->open_cnt = 1;
+  inode->deny_write_cnt = 0;
+  inode->removed = false;
+  lock_init (&inode->lk);
+
+  /* Do not fetch the sector for now. */
+#if 0
+  block_read (fs_device, inode->sector, &inode->data);
+#endif
+
+  /** Release all locks */
+  lock_release (&inode_list_lock);
+  return inode;
+}
+
+/** Reopens and returns INODE. */
+struct inode *
+inode_reopen (struct inode *inode)
+{
+  if (inode != NULL) {
+    lock_acquire (&inode->lk);
+    inode->open_cnt++;
+    lock_release (&inode->lk);
+  }
+  return inode;
+}
+
+/** Returns INODE's inode number. */
+block_sector_t
+inode_get_inumber (const struct inode *inode)
+{
+  return inode->sector;
+}
+
+/** Closes INODE and writes it to disk.
+   If this was the last reference to INODE, frees its memory.
+   If INODE was also a removed inode, frees its blocks. */
+void
+inode_close (struct inode *inode) 
+{
+  /* Ignore null pointer. */
+  if (inode == NULL)
+    return;
+
+  /** Why? because of possible list_remove! */
+  lock_acquire (&inode_list_lock);
+  lock_acquire (&inode->lk);
+
+  /* Release resources if this was the last opener. */
+  if (--inode->open_cnt == 0)
+    {
+      /* Remove from inode list and release lock. */
+      list_remove (&inode->elem);
+ 
+      /* Deallocate blocks if removed. */
+      if (inode->removed) 
+        {
+          inode_deallocate (inode);
+        }
+
+      lock_release (&inode->lk);
+      lock_release (&inode_list_lock);
+      free (inode); 
+      return;
+    }
+
+  lock_release (&inode->lk);
+  lock_release (&inode_list_lock);
+}
+
+/** Marks INODE to be deleted when it is closed by the last caller who
+   has it open. */
+void
+inode_remove (struct inode *inode) 
+{
+  ASSERT (inode != NULL);
+  lock_acquire (&inode->lk);
+  inode->removed = true;
+  lock_release (&inode->lk);
+}
+
+/** Reads SIZE bytes from INODE into BUFFER, starting at position OFFSET.
+   Returns the number of bytes actually read, which may be less
+   than SIZE if an error occurs or end of file is reached. */
+off_t
+inode_read_at (struct inode *inode, void *buffer_, off_t size, off_t offset) 
+{
+  uint8_t *buffer = buffer_;
+  off_t bytes_read = 0;
+
+  /* Fetch and pin the page(DO NOT WRITE inode->data) */
+  const struct inode_disk *sec = bio_read (inode->sector);
+  if (!bio_pin_sec (sec))
+    PANIC ("bio pin");
+
+  /** Hint: enter critical section */
+  lock_acquire (&inode->lk);
+
+  /* If seek outof range, return. */
+  if (offset >= sec->size)
+    goto read_done;
+
+  /* Seek offset */
+  while (size > 0) {
+  }
+
+  /* Done. */
+read_done:
+  if (!bio_unpin_sec (sec))
+    PANIC ("bio unpin");
+  lock_release (&inode->lk);
+  return bytes_read;
+}
+
+/** Disables writes to INODE.
+   May be called at most once per inode opener. */
+void
+inode_deny_write (struct inode *inode) 
+{
+  lock_acquire (&inode->lk);
+  inode->deny_write_cnt++;
+  ASSERT (inode->deny_write_cnt <= inode->open_cnt);
+  lock_release (&inode->lk);
+}
+
+/** Re-enables writes to INODE.
+   Must be called once by each inode opener who has called
+   inode_deny_write() on the inode, before closing the inode. */
+void
+inode_allow_write (struct inode *inode) 
+{
+  lock_acquire (&inode->lk);
+  ASSERT (inode->deny_write_cnt > 0);
+  ASSERT (inode->deny_write_cnt <= inode->open_cnt);
+  inode->deny_write_cnt--;
+  lock_release (&inode->lk);
+}
+
+/** Returns the length, in bytes, of INODE's data. */
+off_t
+inode_length (const struct inode *inode)
+{
+  ASSERT (inode != NULL);
+  off_t ret = 0;
+
+  /* Create critical section */
+  lock_acquire (&inode->lk);
+  const struct inode_disk *di = bio_read (inode->sector);
+  if (!bio_pin_sec (di))
+    PANIC ("bio pin");
+
+  /* read the size data */ 
+  if (di->magic != INODE_MAGIC)
+    PANIC ("not inode sector");
+  ret = di->size;
+
+length_done:
+  if (!bio_unpin_sec (di))
+    PANIC ("bio unpin");
+  lock_release (&inode->lk);
+  return ret;
+}
+
+#endif /**< FILESYS */
